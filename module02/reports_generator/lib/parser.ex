6f6dfba5b5e5fa1f3fbabfd030d1f6471bb92f3d
Mateus Andrade <mat.andrade@live.com>
2021-03-11 18:45:03 -0300

    Moving parser logic into a new module

---
+++ module02/reports_generator/lib/parser.ex
@@ -0,0 +1,17 @@
+defmodule ReportsGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+
+    # |> Enum.map(fn line -> parse_line(line) end)
+    # |> Enum.map(&parse_line(&1))
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
--- module02/reports_generator/lib/reports_generator.ex
+++ module02/reports_generator/lib/reports_generator.ex
@@ -1,15 +1,15 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    # |> Enum.map(fn line -> parse_line(line) end)
-    |> Enum.map(&parse_line(&1))
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report ->
+      sum_values(line, report)
+    end)
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
-  end
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
