3c066c9031310161d475819a14314a4c2a78f071
Mateus Andrade <mat.andrade@live.com>
2021-03-11 08:14:42 -0300

    Added sum_list_test

--- sum_list/test/sum_list_test.exs
+++ sum_list/test/sum_list_test.exs
@@ -1,8 +1,14 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "returns the list's sum" do
+      list = [1, 2, 3, 4]
+
+      response = SumList.call(list)
+      expected_response = 10
+
+      assert response == expected_response
+    end
   end
 end
