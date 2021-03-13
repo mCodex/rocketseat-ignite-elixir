a21ab7935a810fc56a9b87a7579ed0497d9f75ee
Mateus Andrade <mat.andrade@live.com>
2021-03-13 18:06:30 -0300

    Working on parallel report_generator

--- chapter1/module02/challenges/parallel_report_generator/lib/parallel_report_generator.ex
+++ chapter1/module02/challenges/parallel_report_generator/lib/parallel_report_generator.ex
@@ -1,18 +1,74 @@
 defmodule ParallelReportGenerator do
-  @moduledoc """
-  Documentation for `ParallelReportGenerator`.
-  """
+  alias ParallelReportGenerator.{MapMerger, Parser}
 
-  @doc """
-  Hello world.
+  def generate_parallel(filenames) do
+    filenames
+    |> Task.async_stream(&generate_from/1)
+    |> Enum.reduce(initial_acc(), fn {:ok, result}, report ->
+      merge_values(result, report)
+    end)
+  end
 
-  ## Examples
+  # def generate_parallel(filenames) when not is_list(filenames),
+  #   do: {:error, "Provide a list of strings"}
 
-      iex> ParallelReportGenerator.hello()
-      :world
+  defp generate_from(filename) do
+    {:ok, result} =
+      filename
+      |> Parser.parse_file()
 
-  """
-  def hello do
-    :world
+    Enum.reduce(result, initial_acc(), fn line, report ->
+      organize_data(line, report)
+    end)
+  end
+
+  defp merge_values(
+         %{
+           "all_hours" => result_all_hours,
+           "hours_per_month" => result_hours_per_month,
+           "hours_per_year" => result_hours_per_year
+         },
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         }
+       ) do
+    updated_all_hours = MapMerger.merge(result_all_hours, all_hours)
+    updated_hours_per_month = MapMerger.merge(result_hours_per_month, hours_per_month)
+    updated_hours_per_year = MapMerger.merge(result_hours_per_year, hours_per_year)
+
+    build_report(updated_all_hours, updated_hours_per_month, updated_hours_per_year)
+  end
+
+  defp organize_data(
+         [name, hour, _day, month, year],
+         %{
+           :all_hours => all_hours,
+           :hours_per_month => hours_per_month,
+           :hours_per_year => hours_per_year
+         }
+       ) do
+    all_hours = MapMerger.merge(all_hours, %{name => hour})
+
+    hours_per_month = MapMerger.merge_two_levels(hours_per_month, %{name => %{month => hour}})
+
+    hours_per_year = MapMerger.merge_two_levels(hours_per_year, %{name => %{year => hour}})
+
+    %{"all_hours" => all_hours, "hours_per_month" => %{}, "hours_per_year" => %{}}
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp initial_acc() do
+    build_report(%{}, %{}, %{})
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      :all_hours => all_hours,
+      :hours_per_month => hours_per_month,
+      :hours_per_year => hours_per_year
+    }
   end
 end
---
+++ chapter1/module02/challenges/parallel_report_generator/lib/parser.ex
@@ -0,0 +1,35 @@
+defmodule ParallelReportGenerator.Parser do
+  @months %{
+    "1" => "janeiro",
+    "2" => "fevereiro",
+    "3" => "março",
+    "4" => "abril",
+    "5" => "maio",
+    "6" => "junho",
+    "7" => "julho",
+    "8" => "agosto",
+    "9" => "setembro",
+    "10" => "outubro",
+    "11" => "novembro",
+    "12" => "dezembro"
+  }
+
+  def parse_file(filename) do
+    result =
+      "reports/#{filename}"
+      |> File.stream!()
+      |> Stream.map(&parse_line/1)
+
+    {:ok, result}
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &Map.get(@months, &1))
+    |> List.update_at(4, &String.to_integer/1)
+  end
+end
