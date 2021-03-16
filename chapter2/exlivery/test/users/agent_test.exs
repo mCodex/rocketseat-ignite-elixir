820368855c88f2e62912fc6b6f3319f1e8515c26
Mateus Andrade <mat.andrade@live.com>
2021-03-16 20:08:38 -0300

    Added setup on user_agent test

--- chapter2/exlivery/test/users/agent_test.exs
+++ chapter2/exlivery/test/users/agent_test.exs
@@ -18,25 +18,28 @@ defmodule Exlivery.Users.AgentTest do
   end
 
   describe "get/1" do
-    test "when the user is found, returns the user" do
+    setup do
       UserAgent.start_link(%{})
 
+      cpf = "123456"
+
+      {:ok, cpf: cpf}
+    end
+
+    test "when the user is found, returns the user", %{cpf: cpf} do
       :user
-      |> build(cpf: "12345678")
+      |> build(cpf: cpf)
       |> UserAgent.save()
 
-      response = UserAgent.get("12345678")
+      response = UserAgent.get(cpf)
 
       expected_response =
-        {:ok,
-         %User{address: "Street", age: 22, cpf: "12345678", email: "test@test.com", name: "User"}}
+        {:ok, %User{address: "Street", age: 22, cpf: cpf, email: "test@test.com", name: "User"}}
 
       assert response == expected_response
     end
 
     test "when the user is found, returns an error" do
-      UserAgent.start_link(%{})
-
       response = UserAgent.get("123")
 
       expected_response = {:error, "User not found"}
