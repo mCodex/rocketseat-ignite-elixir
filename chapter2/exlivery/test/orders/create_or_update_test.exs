863f17267e5ff78496b1eb926c2135c2c1a7015a
Mateus Andrade <mat.andrade@live.com>
2021-03-18 17:18:16 -0300

    Added order's create_or_update_test

---
+++ chapter2/exlivery/test/orders/create_or_update_test.exs
@@ -0,0 +1,84 @@
+defmodule Exlivery.Orders.CreateOrUpdateTest do
+  use ExUnit.Case
+
+  import Exlivery.Factory
+
+  alias Exlivery.Orders.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  describe "call/1" do
+    setup do
+      cpf = "123456"
+      user = build(:user, cpf: cpf)
+
+      Exlivery.start_agents()
+
+      UserAgent.save(user)
+
+      item1 = %{
+        category: :pizza,
+        description: "Pizza",
+        quantity: 1,
+        unity_price: "35.50"
+      }
+
+      item2 = %{
+        category: :pizza,
+        description: "Pizza 2",
+        quantity: 1,
+        unity_price: "32.00"
+      }
+
+      {:ok, user_cpf: cpf, item1: item1, item2: item2}
+    end
+
+    test "when all params are valid, returns the order", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:ok, _uuid} = response
+    end
+
+    test "when there is no user with given cpf, returns an error", %{
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: "invalid_cpf", items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:error, _message} = response
+    end
+
+    test "when there are invalid items, returns an error", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [%{item1 | quantity: 0}, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid items"}
+
+      assert response == expected_response
+    end
+
+    test "when there are no items, returns an error", %{
+      user_cpf: cpf
+    } do
+      params = %{user_cpf: cpf, items: []}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid items"}
+
+      assert response == expected_response
+    end
+  end
+end
