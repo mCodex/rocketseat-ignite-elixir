33cc920f117112588db1ef0d9f6d700d40545265
Mateus Andrade <mat.andrade@live.com>
2021-03-21 10:23:00 -0300

    Added report generation

---
+++ chapter2/exlivery/lib/orders/report.ex
@@ -0,0 +1,30 @@
+defmodule Exlivery.Orders.Report do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.{Item, Order}
+
+  def create(filename \\ "report.csv") do
+    order_list = build_order_list()
+
+    File.write(filename, order_list)
+  end
+
+  defp build_order_list() do
+    OrderAgent.list_all()
+    |> Map.values()
+    |> Enum.map(&order_string/1)
+  end
+
+  defp order_string(%Order{
+         user_cpf: cpf,
+         items: items,
+         total_price: total_price,
+         delivery_address: delivery_address
+       }) do
+    items_string = Enum.map(items, &item_string/1)
+    "#{cpf}, #{delivery_address}, #{items_string}, #{total_price}\n"
+  end
+
+  defp item_string(%Item{category: category, quantity: quantity, unity_price: unity_price}) do
+    "#{category}, #{quantity}, #{unity_price}, "
+  end
+end
