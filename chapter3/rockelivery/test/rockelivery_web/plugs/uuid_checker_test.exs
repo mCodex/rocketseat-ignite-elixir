bc63a03b5ef04ce222e8ca287a98b49d080e7c2f
Mateus Andrade <mat.andrade@live.com>
2021-04-02 14:20:53 -0300

    Added uuid_checker_test

--- chapter3/rockelivery/test/rockelivery_web/plugs/uuid_checker_test.exs
+++ chapter3/rockelivery/test/rockelivery_web/plugs/uuid_checker_test.exs
@@ -1,8 +1,33 @@
-# defmodule RockeliveryWeb.Plugs.UUIDCheckerTest do
-#   alias RockeliveryWeb.Plugs.UUIDChecker
+defmodule RockeliveryWeb.Plugs.UUIDCheckerTest do
+  use RockeliveryWeb.ConnCase, async: true
 
-#   describe "call/1" do
-#     test "when uuid is invalid returns an error" do
-#     end
-#   end
-# end
+  # alias RockeliveryWeb.Plugs.UUIDChecker
+
+  describe "call/1" do
+    test "when uuid is invalid, go ahead", %{conn: conn} do
+      id = "80546254-2c97-4338-8acc-3e6305d4b523"
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :show, id))
+        |> json_response(:not_found)
+
+      expected_response = %{"message" => "User not found!"}
+
+      assert response == expected_response
+    end
+
+    test "when uuid is invalid returns an error", %{conn: conn} do
+      id = "invalid uuid"
+
+      response =
+        conn
+        |> get(Routes.users_path(conn, :show, id))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid UUID"}
+
+      assert response == expected_response
+    end
+  end
+end
