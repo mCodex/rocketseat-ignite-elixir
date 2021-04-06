7123a44d0dc0e004865f6c9ccd75681513058cbd
Mateus Andrade <mat.andrade@live.com>
2021-04-06 19:00:04 -0300

    Added bypass lib to test external apis calls

--- chapter3/rockelivery/lib/viacep/client.ex
+++ chapter3/rockelivery/lib/viacep/client.ex
@@ -5,12 +5,12 @@ defmodule Rockelivery.ViaCep.Client do
 
   alias Rockelivery.Error
 
-  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws/"
+  @base_url "https://viacep.com.br/ws/"
 
   plug Tesla.Middleware.JSON
 
-  def get_cep_info(cep) do
-    "#{cep}/json/"
+  def get_cep_info(url \\ @base_url, cep) do
+    "#{url}/#{cep}/json/"
     |> get()
     |> handle_get()
   end
